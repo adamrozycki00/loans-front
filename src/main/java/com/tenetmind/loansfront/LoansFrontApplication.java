d891d131d9b982db1d343ee6278c4a9f6a3783de
adamrozycki00 <adamrozycki00@gmail.com>
2021-02-26 21:36:49 +0100

    Initial commit

---
+++ src/main/java/com/tenetmind/loansfront/LoansFrontApplication.java
@@ -0,0 +1,13 @@
+package com.tenetmind.loansfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansFrontApplication.class, args);
+    }
+
+}
