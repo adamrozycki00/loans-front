b98bb6cf0a96d6e80cfd5b0fc7d8cb3ca656742d
adamrozycki00 <adamrozycki00@gmail.com>
2021-02-27 18:11:05 +0100

    Add deleting method for currencies

--- src/main/java/com/tenetmind/loansfront/LoansFrontApplication.java
+++ src/main/java/com/tenetmind/loansfront/LoansFrontApplication.java
@@ -1,13 +1,18 @@
 package com.tenetmind.loansfront;
 
+import com.tenetmind.loansfront.currency.client.CurrencyClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LoansFrontApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoansFrontApplication.class, args);
+        ApplicationContext context = SpringApplication.run(LoansFrontApplication.class, args);
+        CurrencyClient currencyClient = (CurrencyClient) context.getBean("currencyClient");
+        boolean result = currencyClient.deleteCurrency(5L);
+        System.out.println("result: " + result);
     }
 
 }
--- src/main/java/com/tenetmind/loansfront/currency/client/CurrencyClient.java
+++ src/main/java/com/tenetmind/loansfront/currency/client/CurrencyClient.java
@@ -10,6 +10,10 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.singletonList;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 @Component
 public class CurrencyClient {
 
@@ -46,6 +50,30 @@ public class CurrencyClient {
 
         ResponseEntity<CurrencyDto> response =
                 restTemplate.postForEntity(config.getEndpoint(), entity, CurrencyDto.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return true;
+        } else {
+            System.out.println(response.getStatusCode());
+            return false;
+        }
+    }
+
+    public boolean deleteCurrency(Long id) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(APPLICATION_JSON);
+        headers.setAccept(singletonList(APPLICATION_JSON));
+
+        HttpEntity<CurrencyDto> request = new HttpEntity<>(headers);
+
+        ResponseEntity<CurrencyDto> response = restTemplate.exchange(
+                config.getEndpoint() + id,
+                DELETE,
+                request,
+                CurrencyDto.class,
+                1
+        );
 
         if (response.getStatusCode() == HttpStatus.OK) {
             return true;
